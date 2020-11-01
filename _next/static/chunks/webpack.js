1e7230758818208bc8f57c6f725d1f8768c9a4c4
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-11-01 09:34:54 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b1ed3ac162cb6509fbb4";
+/******/ 	var hotCurrentHash = "7a3742eafc86c7a6dd23";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/pages/index.515bc6f88c0200f24dab.hot-update.js
@@ -0,0 +1,276 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./components/badge.tsx":
+false,
+
+/***/ "./components/bmc.tsx":
+false,
+
+/***/ "./components/duplicateTrackList.tsx":
+false,
+
+/***/ "./components/duplicateTrackListItem.tsx":
+false,
+
+/***/ "./components/features.tsx":
+false,
+
+/***/ "./components/footer.tsx":
+false,
+
+/***/ "./components/head.tsx":
+false,
+
+/***/ "./components/intro.tsx":
+false,
+
+/***/ "./components/languageSelector.tsx":
+false,
+
+/***/ "./components/main.tsx":
+false,
+
+/***/ "./components/pages/index.js":
+false,
+
+/***/ "./components/panel.tsx":
+false,
+
+/***/ "./components/reviews.tsx":
+false,
+
+/***/ "./dedup/customFetch.ts":
+false,
+
+/***/ "./dedup/deduplicator.ts":
+false,
+
+/***/ "./dedup/library.ts":
+false,
+
+/***/ "./dedup/oauthConfig.ts":
+false,
+
+/***/ "./dedup/oauthManager.ts":
+false,
+
+/***/ "./dedup/playlistCache.ts":
+false,
+
+/***/ "./dedup/process.ts":
+false,
+
+/***/ "./dedup/promiseForPages.ts":
+false,
+
+/***/ "./dedup/spotifyApi.ts":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/iterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/unsupportedIterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/regenerator/index.js":
+false,
+
+/***/ "./node_modules/next/dist/client/link.js":
+false,
+
+/***/ "./node_modules/next/dist/client/normalize-trailing-slash.js":
+false,
+
+/***/ "./node_modules/next/dist/client/router.js":
+false,
+
+/***/ "./node_modules/next/dist/client/with-router.js":
+false,
+
+/***/ "./node_modules/next/dist/compiled/path-to-regexp/index.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head-manager-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/i18n/normalize-locale-path.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/mitt.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/escape-path-delimiters.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/format-url.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/is-dynamic.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/parse-relative-url.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/path-match.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/prepare-destination.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/querystring.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/resolve-rewrites.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-matcher.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-regex.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/side-effect.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/utils.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/server/denormalize-page-path.js":
+false,
+
+/***/ "./node_modules/next/link.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/arrayLikeToArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/arrayWithHoles.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/assertThisInitialized.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/classCallCheck.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/construct.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/createClass.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/defineProperty.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/getPrototypeOf.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/inherits.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/interopRequireDefault.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/interopRequireWildcard.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/isNativeReflectConstruct.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/iterableToArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/iterableToArrayLimit.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/nonIterableRest.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/possibleConstructorReturn.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/setPrototypeOf.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/slicedToArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/typeof.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/unsupportedIterableToArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/regenerator/index.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/global.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/module.js":
+false,
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var _babel_runtime_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/classCallCheck */ \"./node_modules/@babel/runtime/helpers/esm/classCallCheck.js\");\n/* harmony import */ var _babel_runtime_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/esm/createClass */ \"./node_modules/@babel/runtime/helpers/esm/createClass.js\");\n/* harmony import */ var _babel_runtime_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime/helpers/esm/inherits */ \"./node_modules/@babel/runtime/helpers/esm/inherits.js\");\n/* harmony import */ var _babel_runtime_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime/helpers/esm/possibleConstructorReturn */ \"./node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn.js\");\n/* harmony import */ var _babel_runtime_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime/helpers/esm/getPrototypeOf */ \"./node_modules/@babel/runtime/helpers/esm/getPrototypeOf.js\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"./node_modules/react/jsx-dev-runtime.js\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_5__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_6__);\n/* harmony import */ var _layouts_main__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../layouts/main */ \"./layouts/main.tsx\");\n/* harmony import */ var _i18n__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../i18n */ \"./i18n.js\");\n\n\n\n\n\n\nvar _jsxFileName = \"/Users/jmperez/github/spotify-dedup/pages/index.tsx\";\n\nfunction _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = Object(_babel_runtime_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__[\"default\"])(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = Object(_babel_runtime_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__[\"default\"])(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return Object(_babel_runtime_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_3__[\"default\"])(this, result); }; }\n\nfunction _isNativeReflectConstruct() { if (typeof Reflect === \"undefined\" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === \"function\") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }\n\n\n\n\n\nvar App = /*#__PURE__*/function (_React$Component) {\n  Object(_babel_runtime_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(App, _React$Component);\n\n  var _super = _createSuper(App);\n\n  function App() {\n    Object(_babel_runtime_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(this, App);\n\n    return _super.apply(this, arguments);\n  }\n\n  Object(_babel_runtime_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(App, [{\n    key: \"render\",\n    value: function render() {\n      return /*#__PURE__*/Object(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_5__[\"jsxDEV\"])(_layouts_main__WEBPACK_IMPORTED_MODULE_7__[\"default\"], {}, void 0, false, {\n        fileName: _jsxFileName,\n        lineNumber: 11,\n        columnNumber: 7\n      }, this);\n    }\n  }]);\n\n  return App;\n}(react__WEBPACK_IMPORTED_MODULE_6___default.a.Component);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (App);\n\n;\n    var _a, _b;\n    // Legacy CSS implementations will `eval` browser code in a Node.js context\n    // to extract CSS. For backwards compatibility, we need to check we're in a\n    // browser context before continuing.\n    if (typeof self !== 'undefined' &&\n        // AMP / No-JS mode does not inject these helpers:\n        '$RefreshHelpers$' in self) {\n        var currentExports = module.__proto__.exports;\n        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;\n        // This cannot happen in MainTemplate because the exports mismatch between\n        // templating and execution.\n        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports, module.i);\n        // A module can be accepted automatically based on its exports, e.g. when\n        // it is a Refresh Boundary.\n        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports)) {\n            // Save the previous exports on update so we can compare the boundary\n            // signatures.\n            module.hot.dispose(function (data) {\n                data.prevExports = currentExports;\n            });\n            // Unconditionally accept an update to this module, we'll check if it's\n            // still a Refresh Boundary later.\n            module.hot.accept();\n            // This field is set when the previous version of this module was a\n            // Refresh Boundary, letting us know we need to check for invalidation or\n            // enqueue an update.\n            if (prevExports !== null) {\n                // A boundary can become ineligible if its exports are incompatible\n                // with the previous exports.\n                //\n                // For example, if you add/remove/change exports, we'll want to\n                // re-execute the importing modules, and force those components to\n                // re-render. Similarly, if you convert a class component to a\n                // function, we want to invalidate the boundary.\n                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {\n                    module.hot.invalidate();\n                }\n                else {\n                    self.$RefreshHelpers$.scheduleUpdate();\n                }\n            }\n        }\n        else {\n            // Since we just executed the code for the module, it's possible that the\n            // new exports made it ineligible for being a boundary.\n            // We only care about the case when we were _previously_ a boundary,\n            // because we already accepted this update (accidental side effect).\n            var isNoLongerABoundary = prevExports !== null;\n            if (isNoLongerABoundary) {\n                module.hot.invalidate();\n            }\n        }\n    }\n\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ \"./node_modules/webpack/buildin/harmony-module.js\")(module)))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcGFnZXMvaW5kZXgudHN4P2RiNzYiXSwibmFtZXMiOlsiQXBwIiwiUmVhY3QiLCJDb21wb25lbnQiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFBO0FBR0E7QUFFQTs7SUFFTUEsRzs7Ozs7Ozs7Ozs7Ozs2QkFDSztBQUNQLDBCQUNFLHFFQUFDLHFEQUFEO0FBQUE7QUFBQTtBQUFBO0FBQUEsY0FERjtBQUlEOzs7O0VBTmVDLDRDQUFLLENBQUNDLFM7O0FBUVRGLGtFQUFmIiwiZmlsZSI6Ii4vcGFnZXMvaW5kZXgudHN4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuaW1wb3J0IEluZGV4IGZyb20gJy4uL2NvbXBvbmVudHMvcGFnZXMvaW5kZXgnO1xuaW1wb3J0IFBhZ2UgZnJvbSAnLi4vbGF5b3V0cy9tYWluJztcblxuaW1wb3J0ICcuLi9pMThuJztcblxuY2xhc3MgQXBwIGV4dGVuZHMgUmVhY3QuQ29tcG9uZW50IHtcbiAgcmVuZGVyKCkge1xuICAgIHJldHVybiAoXG4gICAgICA8UGFnZT5cbiAgICAgIDwvUGFnZT5cbiAgICApO1xuICB9XG59XG5leHBvcnQgZGVmYXVsdCBBcHA7XG4iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.tsx\n");
+
+/***/ })
+
+})
