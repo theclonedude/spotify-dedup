33db3850243294137e7bd263a3d2ebc7d866b2de
Jose M. Perez <JMPerez@users.noreply.github.com>
2021-05-11 07:36:08 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ab082b44ac90e757fc63";
+/******/ 	var hotCurrentHash = "befdae618c86964df946";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
